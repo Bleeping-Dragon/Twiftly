7893f0b418fcc5540e00979009f6a1d70ad68854
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2024-04-14 21:58:39 +0200

    Redone bindings.

--- app/src/main/java/com/bleepingdragon/twiftly/WorkingHoursPage.kt
+++ app/src/main/java/com/bleepingdragon/twiftly/WorkingHoursPage.kt
@@ -5,18 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.bleepingdragon.twiftly.databinding.FragmentHomePageBinding
+import com.bleepingdragon.twiftly.databinding.FragmentWorkingHoursPageBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WorkingHoursPage.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WorkingHoursPage : Fragment() {
+
+    //Fragment binding
+    private var _binding: FragmentWorkingHoursPageBinding? = null
+    private val binding get() = _binding!!
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,9 +35,13 @@ class WorkingHoursPage : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_working_hours_page, container, false)
+    ): View {
+        //Inflate the layout for this fragment
+        _binding = FragmentWorkingHoursPageBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
     }
 
     companion object {
