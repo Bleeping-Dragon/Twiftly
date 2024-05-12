6f809a96c96997a59d1e2f1d89f360a0f2d9bb54
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2024-05-12 20:53:22 +0200

    Heads or tails empty page created.

--- app/src/main/java/com/bleepingdragon/twiftly/HeadsTailsPage.kt
+++ app/src/main/java/com/bleepingdragon/twiftly/HeadsTailsPage.kt
@@ -1,30 +1,29 @@
 package com.bleepingdragon.twiftly
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.commit
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.bleepingdragon.twiftly.databinding.FragmentHomePageBinding
+import androidx.navigation.ui.setupWithNavController
+import com.bleepingdragon.twiftly.databinding.FragmentHeadsTailsPageBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class HomePage : Fragment() {
+class HeadsTailsPage : Fragment() {
 
     //Fragment binding
-    private var _binding: FragmentHomePageBinding? = null
+    private var _binding: FragmentHeadsTailsPageBinding? = null
     private val binding get() = _binding!!
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,26 +37,19 @@ class HomePage : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         //Inflate the layout for this fragment
-        _binding = FragmentHomePageBinding.inflate(inflater, container, false)
+        _binding = FragmentHeadsTailsPageBinding.inflate(inflater, container, false)
 
-        //Go to Working hours fragment
-        binding.workingHoursButton.setOnClickListener {
-            view?.findNavController()?.navigate(R.id.action_homePage_to_workingHoursPage) //Use app_navigation.xml action
-        }
-
-        //Go to Compass fragment
-        binding.compassButton.setOnClickListener {
-            view?.findNavController()?.navigate(R.id.action_homePage_to_compassPage) //Use app_navigation.xml action
-        }
-
-        //Go to Compass fragment
-        binding.headsTailsButton.setOnClickListener {
-            view?.findNavController()?.navigate(R.id.action_homePage_to_headsTailsPage) //Use app_navigation.xml action
-        }
+        //Setup the toolbar
+        val navController = findNavController()
+        binding.toolbar.setupWithNavController(navController)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     companion object {
@@ -67,12 +59,12 @@ class HomePage : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomePage.
+         * @return A new instance of fragment WorkingHoursPage.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomePage().apply {
+            HeadsTailsPage().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
