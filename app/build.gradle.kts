7e280abc02be15a555ad89b4142bcdbb7cc1314b
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2024-05-12 21:25:01 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation ("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
