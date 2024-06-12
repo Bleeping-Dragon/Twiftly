08f7394ddda5916347275cf3fa7ee4a532de7520
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2024-06-12 21:23:01 +0200

    Updated to next version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bleepingdragon.twiftly"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0.A"
+        versionCode = 2
+        versionName = "0.1.1.A"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -38,7 +38,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
