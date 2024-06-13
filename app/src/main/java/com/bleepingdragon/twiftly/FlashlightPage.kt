10ad2c0a58ff40190822ebd0a14f9192e47a5243
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2024-06-14 01:02:17 +0200

    Added flashlight tint colors and fixed logic.

--- app/src/main/java/com/bleepingdragon/twiftly/FlashlightPage.kt
+++ app/src/main/java/com/bleepingdragon/twiftly/FlashlightPage.kt
@@ -68,6 +68,7 @@ class FlashlightPage : Fragment() {
 
         //Turn off the flash
         alternateFlash()
+        alternateImages()
 
         //Initialize views
         binding.flashlightFrameView.setOnClickListener {
