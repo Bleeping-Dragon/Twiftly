2c202c409bc65cf04ca3f4ddc40692f1a2a4156b
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2024-12-15 02:46:08 +0100

    Updated dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,14 +38,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation ("androidx.constraintlayout:constraintlayout:2.2.0-alpha14")
-    implementation ("org.osmdroid:osmdroid-android:6.1.18")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
+    implementation ("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation ("org.osmdroid:osmdroid-android:6.1.20")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
