ea009abae6023a9147098a00c4ab76540b3e9b90
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2025-08-27 10:07:30 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bleepingdragon.twiftly"
         minSdk = 30
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.1.4"
+        versionCode = 8
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
